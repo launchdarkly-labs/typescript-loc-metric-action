d6dd6cf485b77525ef00ad65b29eb9782836c3bb
Alexis Georges <alexis.georges@gmail.com>
2020-08-05 11:03:15 -0400

    set up datadog metric logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { promisify } from 'util';
-import { exec as execCb, ExecException } from 'child_process';
+import { exec as execCb } from 'child_process';
+import got from 'got';
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { Context as GithubContext } from '@actions/github/lib/context';
 
 const exec = promisify(execCb);
 
@@ -26,25 +28,52 @@ type ClocOutput = {
   };
 };
 
-async function main() {
+async function submitRatioToDatadog(
+  ratio: number,
+  context: GithubContext,
+  datadogMetric: string,
+  datadogApiKey: string,
+) {
   try {
-    const sourcePath = core.getInput('source-path');
+    const params = new URLSearchParams({ api_key: datadogApiKey });
+    const pushedAt = context.payload['pushed_at'];
+    await got.post(`https://api.datadoghq.com/api/v1/series?${params.toString()}`, {
+      json: {
+        series: [
+          {
+            host: 'gonfalon',
+            metric: datadogMetric,
+            type: 'loc-ratio',
+            points: [[pushedAt, ratio]],
+          },
+        ],
+      },
+      responseType: 'json',
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function reportRatio(sourcePath: string, datadogMetric: string, datadogApiKey: string) {
+  try {
     const { stdout, stderr } = await exec(`npx --quiet cloc --include-lang=TypeScript,JavaScript --json ${sourcePath}`);
 
     if (stderr) {
       throw new Error(stderr);
     }
 
-    const payload = github.context.payload;
-
     const stats = JSON.parse(stdout) as ClocOutput;
     const ratio = stats.TypeScript.code / stats.SUM.code;
 
-    console.log(ratio);
-    console.log(JSON.stringify(payload, null, 2));
+    await submitRatioToDatadog(ratio, github.context, datadogMetric, datadogApiKey);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-main();
+const sourcePath = core.getInput('source-path');
+const datadogMetric = core.getInput('datadog-metric');
+const datadogApiKey = core.getInput('datadog-api-key');
+
+reportRatio(sourcePath, datadogMetric, datadogApiKey);
