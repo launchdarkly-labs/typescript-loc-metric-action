f4d48ec6d5db1ffaf21c7fa4dbeb06fc85dd5495
Alexis Georges <alexis.georges@gmail.com>
2020-08-05 09:39:12 -0400

    switch to async

--- dist/index.js
+++ dist/index.js
@@ -3579,29 +3579,41 @@ module.exports.Collection = Hook.Collection
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const util_1 = __webpack_require__(669);
 const child_process_1 = __webpack_require__(129);
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
-try {
-    const sourcePath = core.getInput('source-path');
-    child_process_1.exec(`npx --quiet cloc --include-lang=TypeScript,JavaScript --json ${sourcePath}`, (err, stdout, stderr) => {
-        if (err) {
-            throw err;
+const exec = util_1.promisify(child_process_1.exec);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const sourcePath = core.getInput('source-path');
+            const { stdout, stderr } = yield exec(`npx --quiet cloc --include-lang=TypeScript,JavaScript --json ${sourcePath}`);
+            if (stderr) {
+                throw new Error(stderr);
+            }
+            const payload = github.context.payload;
+            const stats = JSON.parse(stdout);
+            const ratio = stats.TypeScript.code / stats.SUM.code;
+            console.log(ratio);
+            console.log(JSON.stringify(payload, null, 2));
         }
-        if (stderr) {
-            throw new Error(stderr);
+        catch (error) {
+            core.setFailed(error.message);
         }
-        const payload = github.context.payload;
-        const stats = JSON.parse(stdout);
-        const ratio = stats.TypeScript.code / stats.SUM.code;
-        console.log(ratio);
-        console.log(JSON.stringify(payload, null, 2));
     });
 }
-catch (error) {
-    core.setFailed(error.message);
-}
+main();
 
 
 /***/ }),
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { exec, ExecException } from 'child_process';
+import { promisify } from 'util';
+import { exec as execCb, ExecException } from 'child_process';
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+
+const exec = promisify(execCb);
 
 type ClocOutput = {
   JavaScript: {
@@ -23,29 +26,25 @@ type ClocOutput = {
   };
 };
 
-try {
-  const sourcePath = core.getInput('source-path');
+async function main() {
+  try {
+    const sourcePath = core.getInput('source-path');
+    const { stdout, stderr } = await exec(`npx --quiet cloc --include-lang=TypeScript,JavaScript --json ${sourcePath}`);
 
-  exec(
-    `npx --quiet cloc --include-lang=TypeScript,JavaScript --json ${sourcePath}`,
-    (err: ExecException | null, stdout: string, stderr: string) => {
-      if (err) {
-        throw err;
-      }
+    if (stderr) {
+      throw new Error(stderr);
+    }
 
-      if (stderr) {
-        throw new Error(stderr);
-      }
+    const payload = github.context.payload;
 
-      const payload = github.context.payload;
+    const stats = JSON.parse(stdout) as ClocOutput;
+    const ratio = stats.TypeScript.code / stats.SUM.code;
 
-      const stats = JSON.parse(stdout) as ClocOutput;
-      const ratio = stats.TypeScript.code / stats.SUM.code;
-
-      console.log(ratio);
-      console.log(JSON.stringify(payload, null, 2));
-    },
-  );
-} catch (error) {
-  core.setFailed(error.message);
+    console.log(ratio);
+    console.log(JSON.stringify(payload, null, 2));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
